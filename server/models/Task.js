d4bb97ae2aa41e9c5d9701d1d5806cf28e34bd8c
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-11 11:46:44 -0400

    Commented out so it doesn't run every time

--- server/models/Task.js
+++ server/models/Task.js
@@ -16,19 +16,19 @@ const taskSchema = new Schema({
 const Task = model("Task", taskSchema);
 
 // Clears tasks and reseeds with two generic tasks
-Task.deleteMany({})
-    .then(() => console.log("Tasks deleted."))
-    .then(() => {
-        Task.insertMany([
-            {
-                text: "Task one",
-                username: "JP"
-            },
-            {
-                text: "Task two",
-                username: "Phil"
-            }
-        ]).then(() => console.log("Tasks seeded."));
-    });
+// Task.deleteMany({})
+//     .then(() => console.log("Tasks deleted."))
+//     .then(() => {
+//         Task.insertMany([
+//             {
+//                 text: "Task one",
+//                 username: "JP"
+//             },
+//             {
+//                 text: "Task two",
+//                 username: "Phil"
+//             }
+//         ]).then(() => console.log("Tasks seeded."));
+//     });
 
 module.exports = Task;
